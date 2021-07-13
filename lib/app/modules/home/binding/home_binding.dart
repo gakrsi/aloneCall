8874e29f6dc1b5e615ef695c80816a6fb452dfa4
gauravsingh2708 <gsnaina2708@gmail.com>
2021-07-13 23:02:10 +0530

    bug fix

--- lib/app/modules/home/binding/home_binding.dart
+++ lib/app/modules/home/binding/home_binding.dart
@@ -7,7 +7,7 @@ import 'package:alonecall/app/modules/home/controller/home_controller.dart';
 class HomeBinding extends Bindings{
   @override
   void dependencies() {
-    Get.put<HomeController>(HomeController(), permanent: true,);
+    Get.lazyPut<HomeController>(()=>HomeController());
   }
 
 }
